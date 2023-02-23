e5b321984c41327de42e5237fc80ff7b11d9a8cc
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2023-02-22 20:45:06 -0700

    added test that sorts random array

--- SorterTest.java
+++ SorterTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SorterTest {
 
     @Test
-    public void testMergesort() {
+    public void testMergesortPermuted() {
         for (int k = 0; k < 2000; k++) {
             ArrayList<Integer> arr = new ArrayList<>();
             for (int i = 0; i < k; i++) {
@@ -25,9 +25,23 @@ public class SorterTest {
             for (int i = 0; i < k; i++) {
                 assertEquals(i, arr.get(i));
             }
-
         }
+    }
 
+    @Test
+    public void testMergeSortRandom() {
+        Random rng = new Random();
+        for (int n = 0; n < 2000; n++) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                arr.add(rng.nextInt());
+            }
+            ArrayListSorter.mergesort(arr);
+
+            for (int i = 0; i < arr.size()-1; i++) {
+                assertTrue(arr.get(i).compareTo(arr.get(i+1))<0);
+            }
+        }
     }
 
     @Test
